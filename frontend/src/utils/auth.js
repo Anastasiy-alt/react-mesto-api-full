a76ab37ac520b98fc5c970d9dd0058e14fc1f69e
Анастасия <n060600@yandex.ru>
2022-11-16 19:08:00 +0500

    Build again

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -26,13 +26,13 @@ export const authorize = (email, password) => {
         .then(Api._check)
 };
 
-export const checkToken = () => {
+export const checkToken = (token) => {
     return fetch(`${BASE_URL}/users/me`, {
         method: 'GET',
         headers: {
             // 'Accept': 'application/json',
             'Content-Type': "application/json",
-            // 'Authorization': `Bearer ${token}`
+            'Authorization': `Bearer ${token}`
         },
         credentials: 'include'
     })
