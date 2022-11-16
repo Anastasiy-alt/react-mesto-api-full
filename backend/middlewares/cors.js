3c78b7a8d8fb5a98d3aab04d3beda7d9a241bc06
Анастасия <n060600@yandex.ru>
2022-11-16 18:00:23 +0500

    Update cors.js

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,31 +1,26 @@
 const allowedCors = [
   'https://mesto.pilot.nomoredomains.icu',
+  'http://mesto.pilot.nomoredomains.icu',
   'https://api.mesto.pilot.nomoredomains.icu',
+  'http://api.mesto.pilot.nomoredomains.icu',
   'https://localhost:3000',
+  'http://localhost:3000',
 ];
 
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
 module.exports = (req, res, next) => {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE'; // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-  const requestHeaders = req.headers['access-control-request-headers']; // сохраняем список заголовков исходного запроса
-
-  // проверяем, что источник запроса есть среди разрешённых
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  res.header('Access-Control-Allow-Credentials', true);
   if (allowedCors.includes(origin)) {
-    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
     res.header('Access-Control-Allow-Origin', origin);
-    // устанавливаем заголовок, который разрешает браузеру запросы из любого источника
-    res.header('Access-Control-Allow-Origin', 'true');
   }
-
-  // Если это предварительный запрос, добавляем нужные заголовки
   if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    // разрешаем кросс-доменные запросы с этими заголовками
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    // завершаем обработку запроса и возвращаем результат клиенту
-    return res.end();
+    res.end();
   }
-  return next();
+  next();
 };
