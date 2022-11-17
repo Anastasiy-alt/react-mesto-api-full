2f018dc83042602e90b064a14451d1b11c7b7119
Анастасия <n060600@yandex.ru>
2022-11-17 13:27:32 +0500

    Update users.js

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -93,7 +93,7 @@ module.exports.login = (req, res, next) => {
         NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key',
         { expiresIn: '7d' },
       );
-      res.cookie('jwt', token, {
+      res.cookie('token', token, {
         httpOnly: true,
         sameSite: 'none',
         secure: true,
