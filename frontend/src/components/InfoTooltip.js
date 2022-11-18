0873773cc0f63b9a7a97ff1dbdedd48f1e72b044
Анастасия <n060600@yandex.ru>
2022-11-19 02:13:45 +0500

    Test

--- frontend/src/components/InfoTooltip.js
+++ frontend/src/components/InfoTooltip.js
@@ -7,7 +7,7 @@ function InfoTooltip({ isOpen, onClose, loggedIn }) {
         <div className={`popup popup_for_tooltip ${isOpen && "popup_opened"}`}>
             <div className="popup__container popup__container_for_tooltip">
                 <button className="button popup__close popup__close_for_tooltip" type="button" onClick={onClose}></button>
-                <img src={loggedIn ? yes : no} alt={loggedIn ? "Галочка." : "Крестик."} className="popup__icon" />
+                <img src={loggedIn ? yes : no} alt={loggedIn ? "Галочка." : "Крестик."} className="popup__icon" type="image/svg+xml" />
                 <h3 className="popup__heading popup__heading_for_tooltip">{loggedIn ? "Вы успешно зарегистрировались!" : "Что-то пошло не так! Попробуйте ещё раз"}</h3>
             </div>
         </div>
