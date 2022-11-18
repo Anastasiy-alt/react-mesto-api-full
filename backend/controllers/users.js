edba4a35e849098ead4f6d3b0264fb05cf4ca4d1
Анастасия <n060600@yandex.ru>
2022-11-18 20:40:05 +0500

    Revert "Update users.js"
    This reverts commit f0e98aa0b2bdd0a4655ddecf8f16608b4be0684b.

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -15,11 +15,8 @@ module.exports.getUser = (req, res, next) => {
 };
 
 module.exports.createUser = (req, res, next) => {
-  // const {
-  //   name, about, avatar, email, password,
-  // } = req.body;
   const {
-    password,
+    name, about, avatar, email, password,
   } = req.body;
 
   bcrypt.hash(password, 10)
@@ -30,19 +27,9 @@ module.exports.createUser = (req, res, next) => {
       about: req.body.about,
       avatar: req.body.avatar, // записываем хеш в базу
     }))
-    .then((user) => {
-      const resData = {
-        email: user.email,
-        name: user.name,
-        about: user.about,
-        avatar: user.avatar,
-        _id: user._id,
-      };
-      res.status(201).send(resData);
-    })
-    // .then(() => res.send({
-    //   name, about, avatar, email,
-    // }))
+    .then(() => res.send({
+      name, about, avatar, email,
+    }))
     .catch((err) => {
       if (err.code === 11000) {
         next(new ConflictError('Пользователь с данным email уже существует.'));
@@ -72,8 +59,7 @@ module.exports.updateProfile = (req, res, next) => {
     { name, about },
     { new: true, runValidators: true },
   )
-    // .then((user) => res.send({ user }))
-    .then((user) => res.send(user))
+    .then((user) => res.send({ user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         return next(new BadRequestError('Некорректные данные при создании карточки.'));
@@ -89,8 +75,7 @@ module.exports.updateAvatar = (req, res, next) => {
     { avatar },
     { new: true, runValidators: true },
   )
-    // .then((user) => res.send({ user }))
-    .then((user) => res.send(user))
+    .then((user) => res.send({ user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         return next(new BadRequestError('Некорректные данные при создании карточки.'));
@@ -111,16 +96,18 @@ module.exports.login = (req, res, next) => {
       );
       res.cookie('token', token, {
         httpOnly: true,
+        sameSite: 'none',
+        secure: true,
       });
-      // res.send({
-      //   data: {
-      //     name: user.name,
-      //     avatar: user.avatar,
-      //     about: user.about,
-      //     email: user.email,
-      //     _id: user._id,
-      //   },
-      // });
+      res.send({
+        data: {
+          name: user.name,
+          avatar: user.avatar,
+          about: user.about,
+          email: user.email,
+          _id: user._id,
+        },
+      });
     })
     .catch(next);
 };
@@ -134,16 +121,14 @@ module.exports.logout = (req, res) => {
 };
 
 module.exports.getUserMe = (req, res, next) => {
-  // const { _id } = req.user;
-  const { _id } = req.params;
+  const { _id } = req.user;
   User
     .findById(_id)
     .then((user) => {
       if (!user) {
         throw new NotFoundError('Нет пользователя с таким id.');
       }
-      res.send({ data: user });
-      // res.send(user);
+      res.send(user);
     })
     .catch(next);
 };
