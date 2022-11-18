486c62bafe0ddd0bab52a02e4b4cd26b93b4ade1
Анастасия <n060600@yandex.ru>
2022-11-19 01:39:14 +0500

    Close icon

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -6,7 +6,6 @@ function Card({ card, onCardClick, onCardLike, onCardDelete }) {
     const currentUser = useContext(CurrentUserContext);
     const isOwn = card.owner === currentUser._id;
     const isLiked = card.likes.some((i) => i === currentUser._id);
-    console.log(isOwn, card.owner, currentUser._id);
     const cardDeleteButtonClassName = (
         `${isOwn ? 'element__delete button' : 'element__delete_hidden'}`
     );
